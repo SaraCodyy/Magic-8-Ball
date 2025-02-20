425a18d145802dd267568716adf2ba37982109d0
SaraCodyy <saramostafayosry2007@gmail.com>
2025-02-20 18:16:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-
 responses=[
     'Yes, definitely!!',
     'No,not now',
@@ -10,10 +9,8 @@ responses=[
     'Better not tell you',
     'Concentrate and ask again'
 ]
-
 def generateResponse():
     return random.choice(responses)
-
 def getUserInput():
     question=input("Ask the magical 8 ball a question(type'exit' to quit):")
     if question.lower()=="exit":
@@ -21,6 +18,16 @@ def getUserInput():
     return question
 def display():
     print(generateResponse)
+def playAgain():
+    while True:
+        choice=input("Do you want to ask another question?")
+        if choice=="yes":
+            return True
+        elif choice=='no':
+            print("Thanks for playing good Bye!!")
+            return False
+        else:
+            print("Please Type Yes or no")
 
 
 
